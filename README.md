7830e94f21221a6d89805c9b4ce15d1438744dbc
ari-goldman <112101394+ari-goldman@users.noreply.github.com>
2025-09-07 15:26:26 -0600

    add player

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # atlas-godot-tutorial
+
+## Info
+Tutorial:
+	- Video: https://www.youtube.com/watch?v=GwCiGixlqiU \
+	- Assets: https://www.gdquest.com/library/first_2d_game_godot4_vampire_survivor/#download-files
+
+## Logs
+### 9/3
+- initialized repository with project files
+- 
--- player.gd
+++ player.gd
@@ -6,4 +6,11 @@ const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta):
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * 600
+	move_and_slide()
 	
+	if velocity.length() > 0:
+		%HappyBoo.play_walk_animation()
+	else:
+		%HappyBoo.play_idle_animation()
